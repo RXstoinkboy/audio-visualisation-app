7d18846913fe21c473011b4a1a2f1fcccec5376d
RXstoinkboy <40744540+RXstoinkboy@users.noreply.github.com>
2019-06-03 14:16:11 +0200

    secretly adding IIFE :)
    I have discovered that autoplay has tine issue. IFEE seems to be enough to repair it.

--- src/createAudioElement.js
+++ src/createAudioElement.js
@@ -1,5 +1,6 @@
 const createAudioElement = audio => {
   audio.src = "../bensound-sunny.mp3"; // assign source
+  audio.autoplay = true;
   audio.controls = true; // turn on controls
   audio.crossOrigin = "anonymous"; // prevent error due to CORS
   document.body.appendChild(audio);
--- src/index.js
+++ src/index.js
@@ -3,52 +3,54 @@ import initCanvas from "./initCanvas";
 import setupAudioAPI from "./setupAudioAPI";
 import prepareAudioDataArray from "./prepareAudioDataArray";
 
-// GRAB CANVAS AND CREATE AUDIO ELEMENT
-const canvas = document.getElementById("canvas");
-const audio = new Audio(); // create audio element
-let analyser; // declare variable
+(() => {
+  // GRAB CANVAS AND CREATE AUDIO ELEMENT
+  const canvas = document.getElementById("canvas");
+  const audio = new Audio(); // create audio element
+  let analyser; // declare variable
 
-// setup audio element
-createAudioElement(audio);
+  // setup audio element
+  createAudioElement(audio);
 
-// setup cavnas context
-let ctx = initCanvas(canvas);
+  // setup cavnas context
+  let ctx = initCanvas(canvas);
 
-// create analyser and properly connect input --> analyser --> output
-analyser = setupAudioAPI(analyser, audio);
+  // create analyser and properly connect input --> analyser --> output
+  analyser = setupAudioAPI(analyser, audio);
 
-// define length of array storing audio data
-let bufferLength = analyser.frequencyBinCount;
+  // define length of array storing audio data
+  let bufferLength = analyser.frequencyBinCount;
 
-// assign data to array
-let dataArray = prepareAudioDataArray(bufferLength, analyser);
+  // assign data to array
+  let dataArray = prepareAudioDataArray(bufferLength, analyser);
 
-// paint data as a graph
-const W = canvas.width;
-const H = canvas.height;
-const barWidth = (W / bufferLength) * 80;
-let barHeight; // declare bar height variable
-let x; // declare starting point of each bar
+  // paint data as a graph
+  const W = canvas.width;
+  const H = canvas.height;
+  const barWidth = (W / bufferLength) * 80;
+  let barHeight; // declare bar height variable
+  let x; // declare starting point of each bar
 
-// DRAW VISUALISATION
-function renderFrame() {
-  requestAnimationFrame(renderFrame);
+  // DRAW VISUALISATION
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
 
-  x = 0; // assing 0 as starting point to draw first bar
-  analyser.getByteFrequencyData(dataArray); // copy audio data into array
+    x = 0; // assing 0 as starting point to draw first bar
+    analyser.getByteFrequencyData(dataArray); // copy audio data into array
 
-  ctx.fillStyle = "rgba(0,0,0,0.8)"; // leave a little fade effect
-  ctx.fillRect(0, 0, W, H);
+    ctx.fillStyle = "rgba(0,0,0,0.8)"; // leave a little fade effect
+    ctx.fillRect(0, 0, W, H);
 
-  let bars = 60; // declare FPS
+    let bars = 60; // declare FPS
 
-  for (let i = 0; i < bars; i++) {
-    barHeight = dataArray[i] * 2; // calculate height of each bar;
+    for (let i = 0; i < bars; i++) {
+      barHeight = dataArray[i] * 2; // calculate height of each bar;
 
-    ctx.fillStyle = `hsl(${barHeight}deg, 20%, 40%)`; // assing different color based on bar's height
-    ctx.fillRect(x, H - barHeight, barWidth, barHeight);
-    x += barWidth + 10; // keep some extra space between bars
+      ctx.fillStyle = `hsl(${barHeight}deg, 20%, 40%)`; // assing different color based on bar's height
+      ctx.fillRect(x, H - barHeight, barWidth, barHeight);
+      x += barWidth + 10; // keep some extra space between bars
+    }
   }
-}
 
-renderFrame();
+  renderFrame();
+})();
